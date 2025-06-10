057e061d6ef143ef9f6ad6c77e1c4e6783f85f08
GlinTFraulein <glintfraulein@hotmail.com>
2025-06-10 16:27:55 +0900

    Create OptimizedSetup

--- Editor/ParticleLiveSetup.cs
+++ Editor/ParticleLiveSetup.cs
@@ -12,9 +12,14 @@ namespace PaLASOLU
 {
     public class ParticleLiveSetup : EditorWindow
     {
+        const string basePrefabPath = "Packages/info.glintfraulein.palasolu//Runtime//Prefab/PaLASOLU_Prefab.prefab";
+        
+        AudioClip particleLiveAudio = null;
         string rootFolderName = string.Empty;
-        static bool IsShowAdvancedSettings = false;
+        bool IsShowAdvancedSettings = false;
         bool selectFolder = false;
+        bool moveAudioClip = false;
+        bool existTimeline = false;
 
         [MenuItem("Tools/PaLASOLU/ParticleLive Setup")]
         static void Init()
@@ -27,8 +32,9 @@ namespace PaLASOLU
         {
             GUILayout.Label("パーティクルライブ用フォルダの新規作成", EditorStyles.boldLabel);
             rootFolderName = EditorGUILayout.TextField("フォルダ名(楽曲名を推奨)", rootFolderName);
+            particleLiveAudio = EditorGUILayout.ObjectField("楽曲ファイル(なくても可)", particleLiveAudio, typeof(AudioClip), false) as AudioClip;
 
-            if (GUILayout.Button("フォルダを作成"))
+            if (GUILayout.Button("セットアップ！"))
             {
                 if (rootFolderName == string.Empty)
                 {
@@ -36,7 +42,7 @@ namespace PaLASOLU
                     return;
                 }
 
-                CreateFolders(rootFolderName);
+                OptimizedSetup(rootFolderName);
             }
 
             IsShowAdvancedSettings = EditorGUILayout.Foldout(IsShowAdvancedSettings, "高度な設定");
@@ -44,12 +50,14 @@ namespace PaLASOLU
             {
                 EditorGUI.indentLevel = 1;
                 selectFolder = EditorGUILayout.Toggle("Select Folder Directory", selectFolder);
+                moveAudioClip = EditorGUILayout.Toggle("Move AudioClip File to Particle Live Directory", moveAudioClip);
             }
         }
 
-        void CreateFolders(string rootFolderName)
+        void OptimizedSetup(string rootFolderName)
         {
-            string savePath = Path.Combine("Assets\\ParticleLive", rootFolderName);
+            // Create Folders and Files
+            string savePath = Path.Combine("Assets/ParticleLive", rootFolderName);
 
             if (selectFolder)
             {
@@ -68,22 +76,58 @@ namespace PaLASOLU
                 Debug.LogWarning("[PaLASOLU] 警告 : " + savePath + "フォルダは既に存在します。新しいフォルダは作られません。");
             }
 
-            string assetPath = Path.Combine(savePath, rootFolderName);
+            string timelinePath = Path.Combine(savePath, rootFolderName) + "_timeline.playable";
 
-            if (File.Exists(assetPath + "_timeline.playable"))
+            if (File.Exists(timelinePath))
             {
-                Debug.LogWarning("[PaLASOLU] 警告 : " + assetPath + "_timeline.playable ファイルは既に存在します。新しいファイルは作られません。");
+                Debug.LogWarning("[PaLASOLU] 警告 : " + timelinePath + " ファイルは既に存在します。新しいファイルは作られず、既存のTimelineデータに変更を加えません。");
+                existTimeline = true;
             }
             else
             {
                 var playable = ScriptableObject.CreateInstance<TimelineAsset>();
-                Debug.Log("[PaLASOLU] テストログ " + (playable == null).ToString());
-                AssetDatabase.CreateAsset(playable, assetPath + "_timeline.playable");
-                Debug.Log("[PaLASOLU] ログ(正常) : " + assetPath + "_timeline.playable を作りました。");
+                AssetDatabase.CreateAsset(playable, timelinePath);
+                Debug.Log("[PaLASOLU] ログ(正常) : " + timelinePath + " を作りました。");
+                existTimeline = false;
+            }
+
+            if (moveAudioClip)
+            {
+                var audioClipPath = AssetDatabase.GetAssetPath(particleLiveAudio);
+                AssetDatabase.MoveAsset(audioClipPath, Path.Combine(savePath, Path.GetFileName(audioClipPath)));
             }
 
             AssetDatabase.Refresh();
+
+
+            //Setup Prefab Instance
+            GameObject basePrefab = AssetDatabase.LoadAssetAtPath<GameObject>(basePrefabPath);
+            GameObject plInstance = PrefabUtility.InstantiatePrefab(basePrefab) as GameObject;
+            plInstance.name = rootFolderName + "_ParticleLive";
+
+            GameObject playableTarget = plInstance.transform.Find("WorldFixed/ParticleLive").gameObject;
+            TimelineAsset timeline = AssetDatabase.LoadAssetAtPath<TimelineAsset>(timelinePath);
+            PlayableDirector director = playableTarget.GetComponent<PlayableDirector>();
+            director.playableAsset = timeline;
+
+            if (!existTimeline)
+            {
+                var animationTrack = timeline.CreateTrack<AnimationTrack>();
+                var audioTrack = timeline.CreateTrack<AudioTrack>();
+                director.SetGenericBinding(animationTrack, playableTarget.GetComponent<Animator>());
+
+                if (particleLiveAudio != null)
+                {
+                    TimelineClip audioClipOnTrack = audioTrack.CreateClip<AudioPlayableAsset>();
+                    audioClipOnTrack.displayName = Path.GetFileNameWithoutExtension(particleLiveAudio.name);
+                    audioClipOnTrack.duration = particleLiveAudio.length;
+
+                    AudioPlayableAsset audioAsset = audioClipOnTrack.asset as AudioPlayableAsset;
+                    audioAsset.clip = particleLiveAudio;
+                }
+            }
         }
+
     }
 }
 #endif
