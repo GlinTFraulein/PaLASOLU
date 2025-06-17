bde9f900586ee4bb1bbc4912f1fe8c6805bbb1e1
GlinTFraulein <glintfraulein@hotmail.com>
2025-06-17 21:06:15 +0900

    Add LogMessageSimplifier

---
+++ Editor/LogMessageSimplifier.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace PaLASOLU
+{
+    public class LogMessageSimplifier : MonoBehaviour
+    {
+        public void PaLog(int num, string message)
+        {
+            string returnMessage = "[PaLASOLU] ";
+            if (num == 0) returnMessage += "ログ : ";
+            else if (num == 1) returnMessage += "警告 : ";
+            else if (num == 2) returnMessage += "エラー : ";
+            else
+            {
+                returnMessage += "(これを見つけたら作者 GlinTFraulein に報告！) ";
+
+                if (num == 3) returnMessage += "InternalLog : ";
+                else if (num == 4) returnMessage += "InternalWarning : ";
+                else if (num == 5) returnMessage += "InternalError : ";
+            }
+
+            if (num % 3 == 0) Debug.Log(returnMessage + message);
+            else if (num % 3 == 1) Debug.LogWarning(returnMessage + message);
+            else if (num % 3 == 2) Debug.LogError(returnMessage + message);
+
+            return;
+        }
+    }
+}
