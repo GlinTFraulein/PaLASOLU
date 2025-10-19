990afa50472b86a676ec10e8d3065fbb1d37d6e4
GlinT Fraulein <glintfraulein@hotmail.com>
2025-10-19 16:00:31 +0900

    Add Img2Mat, Mat2Variant

---
+++ Editor/Extensions/Img2Mat.cs
@@ -0,0 +1,147 @@
+﻿/*
+ * Img2Mat
+ *
+ * Click `Tools/PaLASOLU/Extensions/Img2Mat` to open this window.
+ *
+ * zlib License
+ * 
+ * Copyright (c) 2023 anatawa12
+ * 
+ */
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace PaLASOLU
+{
+	internal class Img2Mat : EditorWindow
+	{
+		[MenuItem("Tools/PaLASOLU/Extensions/Img2Mat", priority = 302)]
+		static void Create() => GetWindow<Img2Mat>("Image to Material");
+
+		public Vector2 scrollPos;
+		public Material? referenceMaterial;
+		public Texture?[] textures = Array.Empty<Texture?>();
+		public DefaultAsset? materialsFolder;
+
+		private SerializedObject _serializedObject = null!;
+		private SerializedProperty _referenceMaterialProperty = null!;
+		private SerializedProperty _textureProperty = null!;
+		private SerializedProperty _materialsFolderProperty = null!;
+
+		private void OnEnable()
+		{
+			_serializedObject = new SerializedObject(this);
+			_referenceMaterialProperty = _serializedObject.FindProperty(nameof(referenceMaterial));
+			_textureProperty = _serializedObject.FindProperty(nameof(textures));
+			_materialsFolderProperty = _serializedObject.FindProperty(nameof(materialsFolder));
+		}
+
+		private void OnGUI()
+		{
+			EditorGUILayout.HelpBox("Image to Materialは、1つのマテリアルを基に、テクスチャを差し替えた複数のマテリアルを生成します。", MessageType.None);
+
+			scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+			_serializedObject.Update();
+			EditorGUILayout.PropertyField(_referenceMaterialProperty);
+			EditorGUILayout.PropertyField(_textureProperty, true);
+			EditorGUILayout.PropertyField(_materialsFolderProperty);
+			_serializedObject.ApplyModifiedProperties();
+
+			var hasErrors = false;
+
+			// check if the folder is valid
+			var materialsFolderPath = materialsFolder == null ? null : AssetDatabase.GetAssetPath(materialsFolder);
+			if (string.IsNullOrEmpty(materialsFolderPath) || Directory.Exists(materialsFolderPath) == false)
+			{
+				EditorGUILayout.HelpBox("Materials Folder に、生成したマテリアルの保存先フォルダを設定してください！", MessageType.Error);
+				hasErrors = true;
+			}
+
+			// check if _referenceMaterialProperty is valid
+			if (referenceMaterial == null)
+			{
+				EditorGUILayout.HelpBox("Reference Matelial に、基準となるマテリアルを設定してください！", MessageType.Error);
+				hasErrors = true;
+			}
+
+			EditorGUI.BeginDisabledGroup(hasErrors);
+			if (GUILayout.Button("マテリアル生成！"))
+			{
+				CreateMaterials();
+			}
+			EditorGUI.EndDisabledGroup();
+			EditorGUILayout.EndScrollView();
+		}
+
+		private void CreateMaterials()
+		{
+			if (materialsFolder == null) throw new InvalidOperationException("materialsFolder is null");
+			if (referenceMaterial == null) throw new InvalidOperationException("referenceMaterial is null");
+			var materialsFolderPath = AssetDatabase.GetAssetPath(materialsFolder);
+			if (string.IsNullOrEmpty(materialsFolderPath) || Directory.Exists(materialsFolderPath) == false)
+				throw new InvalidOperationException("materialsFolder is not a valid folder");
+
+			AssetDatabase.StartAssetEditing();
+			try
+			{
+				if (EditorUtility.DisplayCancelableProgressBar("Img2Mat", "Creating materials...", 0f)) return;
+				var existingNames = new HashSet<string>();
+
+				for (var index = 0; index < textures.Length; index++)
+				{
+					var texture = textures[index];
+					if (texture == null) continue;
+					var baseName = texture.name;
+
+					baseName = baseName
+						.Replace('\\', '_')
+						.Replace('/', '_')
+						.Replace(':', '_')
+						.Replace('*', '_')
+						.Replace('?', '_')
+						.Replace('"', '_')
+						.Replace('<', '_')
+						.Replace('>', '_')
+						.Replace('|', '_');
+
+					var materialName = baseName;
+
+					if (!existingNames.Add(materialName))
+					{
+						var increment = 1;
+						for (; ; )
+						{
+							materialName = baseName + "_" + increment++;
+							if (existingNames.Add(materialName)) break;
+						}
+					}
+
+					var progress = (float)index / textures.Length;
+					if (EditorUtility.DisplayCancelableProgressBar("Img2Mat", materialName, progress)) return;
+
+					var material = new Material(referenceMaterial);
+					material.name = materialName;
+					material.mainTexture = texture;
+
+					var materialPath = Path.Combine(materialsFolderPath, materialName + ".mat");
+					AssetDatabase.CreateAsset(material, materialPath);
+					LogMessageSimplifier.PaLog(0, $"{materialPath} 内にマテリアルを生成しました。");
+				}
+			}
+			finally
+			{
+				EditorUtility.ClearProgressBar();
+				AssetDatabase.StopAssetEditing();
+			}
+
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
+		}
+	}
+}
---
+++ Editor/Extensions/Mat2Variant.cs
@@ -0,0 +1,152 @@
+﻿/*
+ * Mat2Variant
+ * 
+ * Click `Tools/PaLASOLU/Extensions/Mat2Variant` to open this window.
+ *
+ * zlib License
+ * 
+ * Copyright (c) 2023 anatawa12
+ * 
+ */
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace PaLASOLU
+{
+	internal class Mat2Variant : EditorWindow
+	{
+
+		[MenuItem("Tools/PaLASOLU/Extensions/Mat2Variant", priority = 303)]
+		static void Create() => GetWindow<Mat2Variant>("Material to Variant");
+
+		public Vector2 scrollPos;
+		public GameObject? referencePrefab;
+		public Material?[] materials = Array.Empty<Material?>();
+		public DefaultAsset? variantsFolder;
+
+		private SerializedObject _serializedObject = null!;
+		private SerializedProperty _referencePrefabProperty = null!;
+		private SerializedProperty _materialsProperty = null!;
+		private SerializedProperty _variantsFolderProperty = null!;
+
+		private void OnEnable()
+		{
+			_serializedObject = new SerializedObject(this);
+			_referencePrefabProperty = _serializedObject.FindProperty(nameof(referencePrefab));
+			_materialsProperty = _serializedObject.FindProperty(nameof(materials));
+			_variantsFolderProperty = _serializedObject.FindProperty(nameof(variantsFolder));
+		}
+
+		private void OnGUI()
+		{
+			EditorGUILayout.HelpBox("Material to Variant は、1つのPrefabを基に、マテリアルを差し替えた複数のPrefab Variantを生成します。", MessageType.None);
+
+			scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+			_serializedObject.Update();
+			EditorGUILayout.PropertyField(_referencePrefabProperty);
+			EditorGUILayout.PropertyField(_materialsProperty);
+			EditorGUILayout.PropertyField(_variantsFolderProperty);
+			_serializedObject.ApplyModifiedProperties();
+
+			var hasErrors = false;
+
+			var variantsFolderPath = variantsFolder == null ? null : AssetDatabase.GetAssetPath(variantsFolder);
+			if (variantsFolderPath == null || !AssetDatabase.IsValidFolder(variantsFolderPath))
+			{
+				EditorGUILayout.HelpBox("Variants Folder に、生成したPrefab Variantの保存先フォルダを設定してください！", MessageType.Error);
+				hasErrors = true;
+			}
+
+			// check if the prefab is valid
+			if (referencePrefab == null || PrefabUtility.GetPrefabAssetType(referencePrefab) == PrefabAssetType.NotAPrefab)
+			{
+				EditorGUILayout.HelpBox("Reference Prefab に、基準となるPrefabを設定してください！", MessageType.Error);
+				hasErrors = true;
+			}
+
+			EditorGUI.BeginDisabledGroup(hasErrors);
+			if (GUILayout.Button("Prefab Variant 生成！"))
+			{
+				CreateVariants();
+			}
+			EditorGUI.EndDisabledGroup();
+			EditorGUILayout.EndScrollView();
+		}
+
+		private void CreateVariants()
+		{
+			if (referencePrefab == null) throw new InvalidOperationException();
+			if (variantsFolder == null) throw new InvalidOperationException();
+			var variantsFolderPath = AssetDatabase.GetAssetPath(variantsFolder);
+			if (string.IsNullOrEmpty(variantsFolderPath) || PrefabUtility.GetPrefabAssetType(referencePrefab) == PrefabAssetType.NotAPrefab)
+				throw new InvalidOperationException();
+
+			AssetDatabase.StartAssetEditing();
+			try
+			{
+				if (EditorUtility.DisplayCancelableProgressBar("Material to Variant", "Creating variants...", 0f)) return;
+				var existingNames = new HashSet<string>();
+
+				for (var index = 0; index < materials.Length; index++)
+				{
+					var material = materials[index];
+					if (material == null) continue;
+
+					var baseName = material.name;
+					baseName = baseName
+						.Replace('\\', '_')
+						.Replace('/', '_')
+						.Replace(':', '_')
+						.Replace('*', '_')
+						.Replace('?', '_')
+						.Replace('"', '_')
+						.Replace('<', '_')
+						.Replace('>', '_')
+						.Replace('|', '_');
+
+					var variantName = baseName;
+
+					if (!existingNames.Add(variantName))
+					{
+						var increment = 1;
+						for (; ; )
+						{
+							variantName = baseName + "_" + increment++;
+							if (existingNames.Add(variantName)) break;
+						}
+					}
+
+					var progress = (float)index / materials.Length;
+					if (EditorUtility.DisplayCancelableProgressBar("Material to Variant", variantName, progress)) return;
+
+					var instance = (GameObject)PrefabUtility.InstantiatePrefab(referencePrefab);
+					var renderer = instance.GetComponentInChildren<Renderer>();
+					if (renderer != null)
+					{
+						renderer.sharedMaterial = material;
+						EditorUtility.SetDirty(renderer);
+						PrefabUtility.RecordPrefabInstancePropertyModifications(renderer);
+					}
+
+					var variantPath = System.IO.Path.Combine(variantsFolderPath, variantName + ".prefab");
+					PrefabUtility.SaveAsPrefabAsset(instance, variantPath);
+					LogMessageSimplifier.PaLog(0, $"{variantPath} 内にPrefab Variantを生成しました。");
+					DestroyImmediate(instance);
+				}
+			}
+			finally
+			{
+				EditorUtility.ClearProgressBar();
+				AssetDatabase.StopAssetEditing();
+			}
+
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
+		}
+	}
+}
