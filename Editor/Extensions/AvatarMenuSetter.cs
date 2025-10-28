d6e36cc8da21ec8aea070b33058d3e19fd077f25
GlinT Fraulein <glintfraulein@hotmail.com>
2025-10-28 23:19:59 +0900

    add AvatarMenuSetter

---
+++ Editor/Extensions/AvatarMenuSetter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+namespace PaLASOLU
+{
+	public class WorldFixedSetter : EditorWindow
+	{
+		const string avatarMenuPath = "Packages/info.glintfraulein.palasolu/Runtime/Prefab/PaLASOLU_v2_Prefab.prefab";
+
+		[MenuItem("Tools/PaLASOLU/Extensions/PaLASOLU Avatar Menu (World Fixed)", priority = 210)]
+		static void SetPaLASOLUAvatarMenu()
+		{
+			GameObject avatarMenuPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(avatarMenuPath);
+			GameObject avatarMenuObject = PrefabUtility.InstantiatePrefab(avatarMenuPrefab) as GameObject;
+			avatarMenuObject.name = "PaLASOLU_AvatarMenu";
+
+			Selection.activeGameObject = avatarMenuObject;
+			EditorUtility.SetDirty(avatarMenuObject);
+		}
+	}
+}
