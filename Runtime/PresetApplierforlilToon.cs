da528527cad9fe9aeb671c03869733f6460f45f0
GlinT Fraulein <glintfraulein@hotmail.com>
2026-02-06 23:51:24 +0900

    fix PAflilToon : upload error

--- Runtime/PresetApplierforlilToon.cs
+++ Runtime/PresetApplierforlilToon.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
+using VRC.SDKBase;
 
 namespace PaLASOLU
 {
 	[AddComponentMenu("PaLASOLU/PaLASOLU PresetApplier for lilToon")]
-	public class PresetApplierforlilToon : MonoBehaviour
+	public class PresetApplierforlilToon : MonoBehaviour, IEditorOnly
 	{
 		/* 嘘実装
 		[Tooltip("適用する lilToon プリセット")]
