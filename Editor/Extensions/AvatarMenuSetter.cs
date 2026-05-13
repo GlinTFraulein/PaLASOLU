09175c6a2ccbbaf01c45420ef133660b089333f1
GlinT Fraulein <glintfraulein@hotmail.com>
2026-05-13 17:39:52 +0900

    add PaLASOLU Playable setter

--- Editor/Extensions/AvatarMenuSetter.cs
+++ Editor/Extensions/AvatarMenuSetter.cs
@@ -7,6 +7,7 @@ namespace PaLASOLU
 	public class WorldFixedSetter : EditorWindow
 	{
 		const string avatarMenuPath = "Packages/info.glintfraulein.palasolu/Runtime/Prefab/PaLASOLU_v2_Prefab.prefab";
+		const string playablePath = "Packages/info.glintfraulein.palasolu/Runtime/Prefab/PaLASOLU_v2_Playable.prefab";
 
 		[MenuItem("Tools/PaLASOLU/Extensions/PaLASOLU Avatar Menu (World Fixed)", priority = 210)]
 		static void SetPaLASOLUAvatarMenu()
@@ -17,6 +18,17 @@ namespace PaLASOLU
 
 			Selection.activeGameObject = avatarMenuObject;
 			EditorUtility.SetDirty(avatarMenuObject);
+		}
+
+		[MenuItem("Tools/PaLASOLU/Extensions/PaLASOLU Prefab", priority = 211)]
+		static void SetPaLASOLUPrefab()
+		{
+			GameObject palasoluPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(playablePath);
+			GameObject palasoluObject = PrefabUtility.InstantiatePrefab(palasoluPrefab) as GameObject;
+			palasoluObject.name = "PaLASOLU_Playable";
+
+			Selection.activeGameObject = palasoluObject;
+			EditorUtility.SetDirty(palasoluObject);
 		}
 	}
 }
